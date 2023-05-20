a26f220ba91d1a0fd1e122dc037d2dc667c2d062
adolfo95villarreal95 <102938659+adolfo95villarreal95@users.noreply.github.com>
2023-05-20 15:58:24 -0600

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/Mibiblioteca/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "control";
+const charset = "charset=utf8";
+?>
